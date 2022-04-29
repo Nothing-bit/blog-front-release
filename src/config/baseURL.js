50bb93da8322d82ca71d18869ebdea88c27fa6ae
ZhouJianGuo <1163959815@qq.com>
2022-04-29 21:39:46 +0800

    V1.8 部分代码重构

--- src/config/baseURL.js
+++ src/config/baseURL.js
@@ -5,7 +5,7 @@ let baseURL
 if (process.env.NODE_ENV === 'development') {
     baseURL = 'http://localhost:8081'
 } else if (process.env.NODE_ENV === 'production') {
-    baseURL = 'http://218.90.120.182:8081'
+    baseURL = 'https://api.zhoujianguo.ltd:8081'
 }
 
 export default baseURL
