c90b947e5fb61e7903873b9e59c5d1c112cb2072
ZhouJianGuo <1163959815@qq.com>
2022-04-29 22:45:58 +0800

    V1.8 部分代码重构

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import ElementUI from 'element-ui'
+// import ElementUI from 'element-ui'
 import vueCookie from 'vue-cookies'
-import 'element-ui/lib/theme-chalk/index.css'
+// import 'element-ui/lib/theme-chalk/index.css'
 import 'highlight.js/styles/atom-one-light.css'
 import router from "./router.js";
 import highlightCustom from "@/utils/highlightCustom";
 
 
-Vue.use(ElementUI)
+// Vue.use(ElementUI)
 Vue.use(vueCookie)
 //过滤所有的高亮模块
 Vue.directive('highlight', {
