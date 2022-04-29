643439ed98606ab91d3c5e70a76a500408c6ac0d
ZhouJianGuo <1163959815@qq.com>
2022-04-29 14:47:21 +0800

    V1.8 部分代码重构

--- vue.config.js
+++ vue.config.js
@@ -7,8 +7,8 @@ module.exports = {
     // 当在非本地环境时，这里以项目test-daily为例，即打包后的h5项目部署服务器的test-daily目录下
     // 那么这里就要把publicPath设置为/test-daily/，表示所有的静态资源都在/test-daily/里
     // 打包部署后，会发现index.html引用的静态资源都添加了路径/test-daily/
-    publicPath: process.env.NODE_ENV == 'development' ? '/' : '/blog-front/',
-    // publicPath:'./',
+    // publicPath: process.env.NODE_ENV == 'development' ? '/' : '/blog-front/',
+    publicPath:'./',
     devServer:{
         port:80, // 启动端口
         open:true,  // 启动后是否自动打开网页
